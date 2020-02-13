0e4f90e552c03da662c995b423acccda706bfdac
Rawaa Rajab <rawaa.rajab@gmail.com>
2020-02-14 01:41:04 +0300

    commit

---
+++ LibraryMangment/src/librarymangment/LibraryMangment.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package librarymangment;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class LibraryMangment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int x,n;
+        n = 10;
+        x= --n-(--n*5);
+        System.out.println(x + " " + n);
+        
+    }
+}
